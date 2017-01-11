d470805d7d8c7b629b8320c0c58b8456be5cfe93
pittNearsoft <projas@nearsoft.com>
2017-01-10 23:51:12 -0700

    Bring back LaunchScreen

--- SmartList/SmartList/AppDelegate.swift
+++ SmartList/SmartList/AppDelegate.swift
@@ -18,9 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Override point for customization after application launch.
     
     window = UIWindow(frame: UIScreen.main.bounds)
-    window?.backgroundColor = UIColor.white
-    window?.rootViewController = ViewController()
     window?.makeKeyAndVisible()
+    window?.rootViewController = UINavigationController(rootViewController: ViewController())
+    
     
     return true
   }
